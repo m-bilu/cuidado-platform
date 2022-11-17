8c9f38b8806b84fea773af74e4d1ccbc0dbb09a4
m-bilu <02.mbilalkhan@gmail.com>
2022-11-16 19:42:04 -0500

    progress on front-end site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileLoginComponent } from './profile-login/profile-login.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path : '', component : HomeComponent },
+  { path : 'services', component : ServicesComponent },
+  { path : 'gallery', component : GalleryComponent },
+  { path : 'contact', component : ContactComponent },
+  { path : 'profile', component : ProfileLoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProfileLoginComponent } from './profile-login/profile-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ServicesComponent,
+    GalleryComponent,
+    ContactComponent,
+    ProfileLoginComponent
   ],
   imports: [
     BrowserModule,
